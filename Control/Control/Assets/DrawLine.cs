98f9bc2620bcaebb04a51d51c847a23d994448a7
Rachel De Paz <rxd678@miami.edu>
2019-06-10 10:40:25 -0400

    Working line renderer in testing scene

--- Control/Control/Assets/DrawLine.cs
+++ Control/Control/Assets/DrawLine.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class DrawLine : MonoBehaviour
 {
 
-    public GameObject origin;
-    public GameObject to; 
+    public Transform origin;
+    public Transform to;
+    private LineRenderer lr; 
 
     void Start()
     {
-        LineRenderer lr = GetComponent<LineRenderer>();
-        lr.SetPosition(0, origin.transform.localPosition);
-        lr.SetPosition(1, to.transform.localPosition);
+        lr = GetComponent<LineRenderer>();
+        lr.SetPosition(0, origin.position);
+        lr.SetWidth(.00f, .016f);
+    }
+
+    void Update()
+    {
+        lr.SetPosition(1, to.position);
     }
 }
