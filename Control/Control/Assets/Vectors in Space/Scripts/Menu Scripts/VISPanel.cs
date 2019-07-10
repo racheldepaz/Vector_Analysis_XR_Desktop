83f217d265fdd369378f0839f994df18935818e9
Rachel De Paz <rxd678@miami.edu>
2019-07-10 14:42:12 -0400

    Finalized menus
    I cant believe I'm almost done...

--- Control/Control/Assets/Vectors in Space/Scripts/Menu Scripts/VISPanel.cs
+++ Control/Control/Assets/Vectors in Space/Scripts/Menu Scripts/VISPanel.cs
@@ -1,11 +1,21 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR.MagicLeap;
 using UnityEngine.UI;
 
 public class VISPanel : MonoBehaviour
 {
     [SerializeField]
     GameObject angleText;  //must be the exact gameobject holding the angle label used in CanvasScript
+
+    [SerializeField]
+    MLInputModule _inputModule = null;
+
+    [SerializeField]
+    LineRenderer _beam = null;
+
+    [SerializeField]
+    Text _angle; 
 
     //Button:Reset.OnClick()
     public void Reset()
@@ -23,8 +33,28 @@ public class VISPanel : MonoBehaviour
     public void ShowAnswers()
     {
         if (angleText.activeSelf == true)
+        {
             angleText.SetActive(false);
+            _angle.text = "Answers: Off";
+        }
         else
+        {
             angleText.SetActive(true);
+            _angle.text = "Answers: On"; 
+        }
+    }
+
+    void Update()
+    {
+        if (_inputModule.PointerLineSegment.End.HasValue)
+        {
+            _beam.enabled = (_inputModule.PointerInput == MLInputModule.PointerInputType.Controller);
+            _beam.SetPosition(0, _inputModule.PointerLineSegment.Start);
+            _beam.SetPosition(1, _inputModule.PointerLineSegment.End.Value);
+        }
+        else
+        {
+            _beam.enabled = false;
+        }
     }
 }
